83807e5749a8fea26626eb42bca54e081f9730f9
Moody Rahman <moody.byte@gmail.com>
2021-04-17 20:39:16 -0400

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+
+from flask import *
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        pass
